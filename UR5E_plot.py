0fda49df8bc69d74190a690bdb07e9cf5ec124d8
Jiyoung Choi <77952928+JIYOUNG3347@users.noreply.github.com>
2023-12-06 17:09:41 +0900

    Update UR5E_plot.py

--- UR5E_plot.py
+++ UR5E_plot.py
@@ -11,7 +11,7 @@ from pytransform3d.plot_utils import Frame
 
 wd = os.path.abspath(os.getcwd())
 sys.path.append(str(wd))
-sys.path.append("/home/user/cable_casting_4.4.0/VREP_4.4.0/programming/zmqRemoteApi/clients/python")
+sys.path.append("./clients/python")
 import numpy as np
 from zmqRemoteApi import RemoteAPIClient
 
